7d0bfa6d7f41d8814d8f804e399646c86d7cd53a
satish patel <155558572+patelsatish25@users.noreply.github.com>
2025-07-25 23:29:56 +0530

    feat: return 400 for missing fields in Register API

--- backend/src/controllers/Register.js
+++ backend/src/controllers/Register.js
@@ -1,5 +1,11 @@
 const Register = (req, res) => {
-             
+    const { username, name, phone} = req.body;
+  
+    // 1. Check for missing fields
+    if (!username || !name || !phone) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+    
 };
   
   module.exports = Register;
