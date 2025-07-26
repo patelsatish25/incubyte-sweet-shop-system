12c58064dbe4822d57b2c6c37ab5878b15c5c4f2
satish patel <155558572+patelsatish25@users.noreply.github.com>
2025-07-26 20:42:50 +0530

    feat: implement sweet creation with admin access control and validation POST /api/sweets

--- backend/src/controllers/Sweets.js
+++ backend/src/controllers/Sweets.js
@@ -8,7 +8,33 @@ const addSweets=async(req,res)=>
     const role = req.headers["role"];
 
     console.log("📌 Role from header:", role);
- 
+  try{
+    // Allow only if user is admin
+    if (role !== "admin") {
+      return res.status(403).json({ error: "Access denied. Admins only." });
+    }
+    const { sweetId, name, category, price, quantityInStock } = req.body;
+
+    if (!sweetId || !name || !category || !price || !quantityInStock ) {
+      return res.status(400).json({ error: "Missing required sweet fields" });
+    }
+    // Save the sweet
+    const newSweet = new Sweets({
+        sweetId,  // You need to add this field in schema too
+        name,
+        category,
+        price,
+        quantityInStock
+      });
+  
+      await newSweet.save();
+      
+      return res.status(201).json({ message: "Sweet added successfully", sweet: newSweet });
+  
+    } catch (error) {
+      console.error("❌ Error while adding sweet:", error.message);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
 
 
 }
--- backend/src/uthetication.js
+++ backend/src/uthetication.js
@@ -10,7 +10,10 @@ const veryfieauth=(req,res,next)=>{
        
          
 
-     
+       if(err)
+       {
+        return res.status(403).json({ error: "Invalid token" });
+       }
         req.user=user;
         
         next();
